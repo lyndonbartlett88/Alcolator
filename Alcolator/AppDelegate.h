81c8ca06b7aba8062749d8bc9995b03266e6b9cb
Lyndon <lyndon@ksbartlett.com>
2015-09-22 21:00:14 -0500

    Initial Commit

---
+++ Alcolator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alcolator
+//
+//  Created by Lyndon Bartlett on 9/22/15.
+//  Copyright © 2015 Lyndon Bartlett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
