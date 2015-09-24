3f443f99bbc0bde83dcecd02be6c48af478382c7
Lyndon <lyndon@ksbartlett.com>
2015-09-24 15:35:17 -0500

    Alcolator Second Part
    Second Part Complete

---
+++ Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Lyndon Bartlett on 9/24/15.
+//  Copyright © 2015 Lyndon Bartlett. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MainMenuViewController : ViewController
+
+@end
